a69b5dba21cb2342756710b935200c157ccdcd13
sachin <90228537prasuramy@gmail>
2025-07-23 15:08:16 +0530

    create a redisServies files

--- src/main/java/net/engineeringdigest/journalApp/Service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/Service/WeatherService.java
@@ -21,17 +21,32 @@ public class WeatherService {
     @Value("${Weather.api.key}")
     private  String apikey;
 
+    @Autowired
+    private RedisService redisService;
 
 
     public  WeatherResponce getWeather(String city)
     {
+        WeatherResponce weatherResponce = redisService.get("weather_of_ " + city, WeatherResponce.class);
+        if (weatherResponce != null)
+        {
+            return weatherResponce;
+        }
+        else {
+            String finalapi =appCache.AppCache.get(AppCache.keys.WEATHER_API.toString()).replace("<city>",city).replace("<apikey>",apikey);
+            ResponseEntity<WeatherResponce> responce =restTemplate.exchange(finalapi, HttpMethod.GET, null, WeatherResponce.class);
+            WeatherResponce body = responce.getBody();
+            if (body !=null)
+            {
+                redisService.set("weather_of_ "+city,body,300l);
+            }
+            return body;
 
-        String finalapi =appCache.AppCache.get(AppCache.keys.WEATHER_API.toString()).replace("<city>",city).replace("<apikey>",apikey);
-        ResponseEntity<WeatherResponce> responce =restTemplate.exchange(finalapi, HttpMethod.GET, null, WeatherResponce.class);
-        WeatherResponce body = responce.getBody();
-        return body;
+        }
+
 
     }
+
 
 
 }
